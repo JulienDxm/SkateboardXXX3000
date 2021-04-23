9ac742c50a04f093442bfaef3946f1fa826a6937
libcraps <56928268+libcraps@users.noreply.github.com>
2021-04-23 15:41:55 +0200

    Read Csv up in untiy
    Gonna try data table

---
+++ 02 - Unity/SkateboardXXX3000_visu_unity/Assets/Scripts/MovuinoDataSet.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Movuino
+{
+    public class MovuinoDataSet
+    {
+
+        private string dataPath;
+        List<object[]> rawData = new List<object[]>();
+
+        object[] time;
+        object[] acceleration;
+        object[] gyroscope;
+        object[] magnetometer;
+
+        object[] theta;
+        object[] velocity;
+        object[] pos;
+
+        float[] normAccel;
+        float[] normGyr;
+
+
+        public MovuinoDataSet(string dataPath)
+        {
+            Debug.Log("Reading... " + dataPath);
+            rawData = ReadCSV(dataPath);
+
+            
+        }
+
+        List<object[]> ReadCSV(string dataPath)
+        {
+
+            StreamReader sr = new StreamReader(dataPath);
+            char sep = ',';
+
+            string line = sr.ReadLine();
+
+            //We'ra counting the number of column
+            int nb_columne = 1;
+            foreach (char a in line)
+            {
+                if (a == sep)
+                    nb_columne++;
+            }
+            List<object[]> data = new List<object[]>(); //All the file
+            object[] tData = new object[nb_columne]; //Data for a t time that has a 
+            string value = "";
+            int i = 0;
+
+            //Header
+            foreach (char a in line)
+            {
+                if (a == ',')
+                {
+                    tData[i] = value;
+                    
+                    value = "";
+                    i += 1;
+                }
+                else
+                {
+                    value += a;
+                }
+
+            }
+            tData[i] = value;
+            data.Add(tData);
+            tData = new object[nb_columne];
+            value = "";
+            line = sr.ReadLine();
+
+            //Data
+            while (line != null)
+            {
+                i = 0;
+                //We read all the line
+                foreach (char a in line)
+                {
+                    if (a == ',')
+                    {
+                        tData[i] = float.Parse(value, CultureInfo.InvariantCulture);
+                        value = "";
+                        i += 1;
+                    }
+                    else
+                    {
+                        value += a;
+                    }
+
+                }
+                tData[i] = float.Parse(value, CultureInfo.InvariantCulture);
+                data.Add(tData);
+                tData = new object[nb_columne];
+                value = "";
+                line = sr.ReadLine();
+            }
+            Debug.Log(data[0][1]);
+            return data;
+        }
+
+
+
+    }
+
+
+}
--- 02 - Unity/SkateboardXXX3000_visu_unity/Assets/Scripts/SkateBehaviour.cs
+++ 02 - Unity/SkateboardXXX3000_visu_unity/Assets/Scripts/SkateBehaviour.cs
@@ -3,94 +3,60 @@ using System.IO;
 using System.Globalization;
 using System.Collections;
 using System.Collections.Generic;
+using Movuino;
 using UnityEngine;
 
-public class SkateBehaviour : MonoBehaviour
+namespace Skateboard
 {
-    private string dataPath = ".\\Data_visu\\Movuino-heel_50HZ_smooth15treated.csv";
-    List<float[]> rawData = new List<float[]>();
-    Dictionary<string, List<float>> completeCSV = new Dictionary<string, List<float>>();
-    float startTime;
-    int i;
-    bool end;
 
-    float[] time;
-    Vector3[] acceleration;
-    Vector3[] gyroscope;
-    Vector3[] magnetometer;
-
-    Vector3[] theta;
-    Vector3[] velocity;
-    Vector3[] pos;
-
-    float[] normAccel;
-    float[] normGyr;
-    void Start()
+    public class SkateBehaviour : MonoBehaviour
     {
-        rawData = ReadCSV(dataPath);
-        time = rawData[0];
+        private MovuinoDataSet movuinoDataSet;
 
-        i = 0;
-        startTime = Time.time;
-        end = false;
-        //this.gameObject.transform.Rotate(new Vector3(5, 0, 0));
+        private string dataPath = ".\\Data_visu\\Movuino-heel_50HZ_smooth15treated.csv";
 
-    }
-    private void FixedUpdate()
-    {
-        //if (i < rawData.Count && Time.time - startTime >= rawData[i][0] * 0.001 && Time.time - startTime <= rawData[i + 1][0] * 0.001)
-        if (i < rawData.Count-1)
+        private float startTime;
+        private int i;
+        private bool end;
+
+
+        private void Start()
         {
-            //Vector3 velocity = new Vector3(-rawData[i][13] * (float)0.5, -rawData[i][14] * (float)0.5, -rawData[i][15] * (float)0.5);
-            //this.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(rawData[i][1], rawData[i][3], rawData[i][2]), ForceMode.Acceleration);
-            float deltaRotX = (rawData[i + 1][10] - rawData[i][10]) * (float)(360 / 0.25);
-            float deltaRotY = (rawData[i + 1][11] - rawData[i][11]) * (float)(360 / 0.25);
-            float deltaRotZ = (rawData[i + 1][12] - rawData[i][12]) * (float)(360 / 0.25);
-            this.gameObject.transform.Rotate(new Vector3(deltaRotX, deltaRotY, deltaRotZ));
-
-            //this.gameObject.transform.Translate(velocity);
-            //this.gameObject.transform.Rotate(new Vector3(rawData[i][4] * (float)(360 / 0.25), rawData[i][5] * (float)(360 / 0.25), rawData[i][6] * (float)(360 / 0.25)));
-            i += 1;
+            movuinoDataSet = new MovuinoDataSet(dataPath);
         }
 
+
     }
 
-
-    List<float[]> ReadCSV(string dataPath)
-    {
-        StreamReader sr = new StreamReader(dataPath);
-
-        List<float[]> data = new List<float[]>();
-        float[] tData = new float[21];
-        string value = "";
-
-        string line = sr.ReadLine();
-        line = sr.ReadLine();
-        int i = 0;
-
-        while (line != null)
-        {
-            foreach (char a in line)
-            {
-                if (a == ',')
-                {
-                    tData[i] = float.Parse(value, CultureInfo.InvariantCulture);
-                    Debug.Log(i);
-                    value = "";
-                    i += 1;
-                } 
-                else
-                {
-                    value += a;
-                }
-                
-            }
-            i = 0;
-            data.Add(tData);
-            tData = new float[21];
-            value = "";
-            line = sr.ReadLine();
-        }
-        return data;
-    }
 }
+
+/*
+void Start()
+{
+    movuinoDataSet = new MovuinoDataSet(dataPath);
+    i = 0;
+    startTime = Time.time;
+    end = false;
+    //this.gameObject.transform.Rotate(new Vector3(5, 0, 0));
+
+
+
+}
+private void FixedUpdate()
+{
+
+    //if (i < rawData.Count && Time.time - startTime >= rawData[i][0] * 0.001 && Time.time - startTime <= rawData[i + 1][0] * 0.001)
+    if (i < rawData.Count - 1)
+    {
+        //Vector3 velocity = new Vector3(-rawData[i][13] * (float)0.5, -rawData[i][14] * (float)0.5, -rawData[i][15] * (float)0.5);
+        //this.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(rawData[i][1], rawData[i][3], rawData[i][2]), ForceMode.Acceleration);
+        float deltaRotX = (rawData[i + 1][10] - rawData[i][10]) * (float)(360 / 0.25);
+        float deltaRotY = (rawData[i + 1][11] - rawData[i][11]) * (float)(360 / 0.25);
+        float deltaRotZ = (rawData[i + 1][12] - rawData[i][12]) * (float)(360 / 0.25);
+        this.gameObject.transform.Rotate(new Vector3(deltaRotX, deltaRotY, deltaRotZ));
+
+        //this.gameObject.transform.Translate(velocity);
+        //this.gameObject.transform.Rotate(new Vector3(rawData[i][4] * (float)(360 / 0.25), rawData[i][5] * (float)(360 / 0.25), rawData[i][6] * (float)(360 / 0.25)));
+        i += 1;
+    }
+    */
