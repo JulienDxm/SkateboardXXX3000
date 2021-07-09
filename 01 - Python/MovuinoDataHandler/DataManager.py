e6cde8fddea8e5af03270b44d37cb60af528e328
libcraps <56928268+libcraps@users.noreply.github.com>
2021-07-09 15:09:44 +0200

    comit

--- 01 - Python/MovuinoDataHandler/DataManager.py
+++ 01 - Python/MovuinoDataHandler/DataManager.py
@@ -188,7 +188,7 @@ class MovuinoDataSet():
         g = plt.subplot(332)
         g.plot(self.time, self.gyroscope[:, 0], color="r")
         g.plot(self.time, self.gyroscope[:, 1], color="green")
-        g.plot(self.time, self.gyroscope[:, 2], color="blue", marker=".")
+        g.plot(self.time, self.gyroscope[:, 2], color="blue")
         g.set_title('Gyroscope (deg/s)')
 
         m = plt.subplot(333)
@@ -236,7 +236,7 @@ class MovuinoDataSet():
         patchX = mpatches.Patch(color='red', label='x')
         patchY = mpatches.Patch(color='green', label='y')
         patchZ = mpatches.Patch(color='blue', label='z')
-        plt.legend(handles=[patchX, patchY, patchZ], loc="center right", bbox_to_anchor=(2.5,2.4),ncol=1)
+        plt.legend(handles=[patchX, patchY, patchZ], loc="center right", bbox_to_anchor=(-2.5, 3.6), ncol=1)
         plt.show()
 
     def TrapezeIntegration(self, y1, y2, t1, t2, dt=0):
@@ -288,114 +288,16 @@ def BandPassButterworthFilter(order,cutsLim,sig):
 
 def BandPassFilter():
     return 0
-"""
-class MovuinoDataSetThread(Thread):
 
-    def __init__(self, filepath):
-
-        self.filepath = filepath
-        self.rawData = pd.read_csv(filepath + ".csv", sep=",")
-
-        self.time = []
-
-        # liste de vecteurs numpy
-        self.acceleration = []
-        self.gyroscope = []
-        self.magnetometer = []
-
-        self.normAcceleration = []
-        self.normGyroscope = []
-        self.normMagnetometer = []
-
-        self.velocity = [np.array([0, 0, 0])]
-        self.pos = [np.array([0, 0, 0])]
-        self.posAng = [np.array([0, 0, 0])]
-
-        posAngX = 0
-        posAngY = 0
-        posAngZ = 0
-
-        posX = 0
-        posY = 0
-        posZ = 0
-
-        vx = 0
-        vy = 0
-        vz = 0
-
-        self.time = list(self.rawData["time"])
-
-        self.nb_line = len(self.time)
-
-        self.compteur_line = 0
-        self.thread = Thread.__init__(self)
-
-    def run(self):
-        for k in range(self.nb_line):
-            self.acceleration.append(np.array([self.rawData["ax"][k], self.rawData["ay"][k], self.rawData["az"][k]]))
-            self.gyroscope.append(np.array([self.rawData["gx"][k], self.rawData["gy"][k], self.rawData["gz"][k]]))
-            self.magnetometer.append(np.array([self.rawData["mx"][k], self.rawData["my"][k], self.rawData["mz"][k]]))
-
-            self.normAcceleration.append(np.linalg.norm(self.acceleration[k]))
-            self.normGyroscope.append(np.linalg.norm(self.gyroscope[k]))
-
-            #---- integration -----
-            if k < self.nb_line-1:
-                pas = self.time[k+1] - self.time[k]
-
-                posAngX = self.gyroscope[k][0]*pas*0.001*180/np.pi + self.posAng[k][0]  # 360/2np.pi
-                posAngY = self.gyroscope[k][1]*pas*0.001*180/np.pi + self.posAng[k][1]
-                posAngZ = self.gyroscope[k][2]*pas*0.001*180/np.pi + self.posAng[k][2]
-                self.posAng.append(np.array([posAngX, posAngY, posAngZ]))
-
-                vx = self.acceleration[k][0]*pas*0.001*180/np.pi + self.velocity[k][0]  # 360/2np.pi
-                vy = self.acceleration[k][1]*pas*0.001*180/np.pi + self.velocity[k][1]
-                vz = self.acceleration[k][2]*pas*0.001*180/np.pi + self.velocity[k][2]
-                self.posAng.append(np.array([vx, vy, vz]))
-
-                posX = self.velocity[k][0]*pas*0.001*180/np.pi + self.posX[k][0]  # 360/2np.pi
-                posY = self.velocity[k][1]*pas*0.001*180/np.pi + self.posY[k][1]
-                posZ = self.velocity[k][2]*pas*0.001*180/np.pi + self.posZ[k][2]
-                self.pos.append(np.array([posX, posY, posZ]))
+def Butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs  # sampling frequency
+    low = lowcut / nyq
+    high = highcut / nyq
+    sos = signal.butter(order, [low, high], analog=False, btype='band', output='sos')
+    return sos
 
 
-
-            if k == self.nb_line-1:
-                self.ConvertArray()
-                self.StockIntoNewFile()
-                self.plotImage()
-
-    def ConvertArray(self):
-        self.acceleration = np.array(self.acceleration)
-        self.gyroscope = np.array(self.gyroscope)
-        self.magnetometer = np.array(self.magnetometer)
-
-        self.posAng = np.array(self.posAng)
-        self.pos = np.array(self.pos)
-        self.velocity = np.array(self.velocity)
-
-        self.rawData["normAccel"] = self.normAcceleration
-        self.rawData["normGyr"] = self.normGyroscope
-
-    def StockIntoNewFile(self):
-        self.rawData.to_csv(self.filepath + "_treated" + ".csv", sep=",", index=False, index_label=False)
-
-    def plotImage(self):
-
-        a = plt.subplot(331)
-
-        a.plot(self.time, self.acceleration[:, 0], color = "r")
-        a.plot(self.time, self.acceleration[:, 1], color = "green")
-        a.plot(self.time, self.acceleration[:, 2], color = "blue")
-        g.set_title('Acceleration')
-
-        g = plt.subplot(332)
-        g.plot(self.time, self.self.gyroscope[:, 0], color="r")
-        g.plot(self.time, self.self.gyroscope[:, 1], color="green")
-        g.plot(self.time, self.self.gyroscope[:, 2], color="blue")
-        g.set_title('Gyroscope')
-        plt.show()
-
-    def TrapezeIntegration(self, y1, y2, t1, t2, dt=0):
-        return np.trapz([y1, y2], [t1, t2])
-"""
+def Butter_bandpass_filter(sig, lowcut, highcut, fs, order=5):
+    sos = Butter_bandpass(lowcut, highcut, fs, order=order)
+    sig_filtered = signal.sosfilt(sos, sig)
+    return sig_filtered
--- 01 - Python/MovuinoDataHandler/main.py
+++ 01 - Python/MovuinoDataHandler/main.py
@@ -7,20 +7,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
-def butter_bandpass(lowcut, highcut, fs, order=5):
-        nyq = 0.5 * fs #sampling frequency
-        low = lowcut / nyq
-        high = highcut / nyq
-        sos = signal.butter(order, [low, high], analog=False, btype='band', output='sos')
-        return sos
-
-def butter_bandpass_filter(sig, lowcut, highcut, fs, order=5):
-        sos = butter_bandpass(lowcut, highcut, fs, order=order)
-        sig_filtered = signal.sosfilt(sos, sig)
-        return sig_filtered
-
-folderPath = "..\\..\\Data\\Cri_tricks_stationary_5_goodOne\\Only_tricks_notransition\\"
-#folderPath = "..\\..\\Data\\Movuino-heel_50HZ_smooth15\\"
+folderPath = "..\\..\\Data\\4polo_2\\"
+folderPath = "..\\..\\Data\\Movuino-heel_50HZ_smooth15\\"
 fileName = "record"
 
 path = folderPath + fileName
@@ -75,72 +63,11 @@ if toExtract:
 
 if toDataManage:
     print(nbRecord)
-    nbRecord = 11
+    nbRecord = 12
     for i in range(1, nbRecord+1):
         dataSet = dm.MovuinoDataSet(folderPath + fileName + "_"+str(i))
         dataSet.run()
         Te = dataSet.Te
         print(1/Te)
-    """
 
-    a_pb = butter_bandpass_filter(dataSet.acceleration[:, 2], 0.15, 4.5, 1/Te, order = 3)
-    a_pb_butter = dm.BandPassButterworthFilter(4, [0.3, 0.9], dataSet.acceleration[:, 2])
-
-    ax_pb = dm.LowPassFilter(dataSet.time, dataSet.acceleration[:, 2], dataSet.time[-1] / len(dataSet.time), fc=0.001)
-    ax_pb_2 = dm.LowPassButterworthFilter(3, 0.3, dataSet.acceleration[:, 2])
-
-    plt.plot(dataSet.time, dataSet.acceleration[:, 2], color='silver', label='Signal')
-    plt.plot(dataSet.time, a_pb, color='#cc0000', label='Signal filtré passe bande')
-    plt.plot(dataSet.time, a_pb_butter, color='blue', label='Signal filtré passe bande')
-    plt.grid(True, which='both')
-    plt.legend(loc="best")
-    plt.title("Filtre passe-bas du 1er ordre")
-    plt.show()
-
-    plt.plot(dataSet.time, dataSet.acceleration[:, 2], color='silver', label='Signal')
-    plt.plot(dataSet.time, ax_pb, color='#cc0000', label='Signal filtré passe bande')
-    plt.plot(dataSet.time, ax_pb_2, color='blue', label='Signal filtré passe bande')
-    plt.grid(True, which='both')
-    plt.legend(loc="best")
-    plt.title("Filtre passe-bas du 1er ordre")
-    plt.show()
-
-    for i in range(nbRecord):
-        dataSet = dm.MovuinoDataSet(path + "_" + str(i+1))
-        dataSet.run()
-    """
-    """
-        ax_pb = dm.LowPassFilter(dataSet.time, dataSet.acceleration[:,0], dataSet.time[-1]/len(dataSet.time), fc = 0.05)
-        ax_pb_2 = dm.LowPassButterworthFilter(3, 0.09, dataSet.acceleration[:,0])
-
-
-        g_pb = []
-        g_pb_butter = []
-        g = [dataSet.gyroscope[:,0],dataSet.gyroscope[:,1],dataSet.gyroscope[:,2]]
-        g_pb.append(dm.LowPassFilter(dataSet.time, dataSet.gyroscope[:,0], dataSet.time[-1]/len(dataSet.time), fc = 1))
-        g_pb_butter.append(dm.LowPassButterworthFilter(5, 0.9, dataSet.gyroscope[:,0]))
-        g_pb.append(dm.LowPassFilter(dataSet.time, dataSet.gyroscope[:,1], dataSet.time[-1]/len(dataSet.time), fc = 1))
-        g_pb_butter.append(dm.LowPassButterworthFilter(5, 0.9, dataSet.gyroscope[:,1]))
-        g_pb.append(dm.LowPassFilter(dataSet.time, dataSet.gyroscope[:,2], dataSet.time[-1]/len(dataSet.time), fc = 1))
-        g_pb_butter.append(dm.LowPassButterworthFilter(5, 0.9, dataSet.gyroscope[:,2]))
-
-        disp.Display("Gyroscope filtré", dataSet.time, g, g_pb, g_pb_butter)
-
-        a_pb = []
-        a_pb_butter = []
-        a = [dataSet.acceleration[:, 0], dataSet.acceleration[:, 1], dataSet.acceleration[:, 2]]
-        a_pb_butter.append(dm.BandPassButterworthFilter(4, [0.1, 0.3], dataSet.acceleration[:, 0]))
-        a_pb_butter.append(dm.BandPassButterworthFilter(4, [0.1, 0.3], dataSet.acceleration[:, 1]))
-        a_pb_butter.append(dm.BandPassButterworthFilter(4, [0.1, 0.15], dataSet.acceleration[:, 2]))
-        disp.Display("Acceleration", dataSet.time, a, a_pb_butter)
-        #disp.Display("Gyroscope filtré", dataSet.time, g, g_pb, g_pb_butter)
-        
-        plt.plot(dataSet.time, dataSet.acceleration[:,0], color='silver', label='Signal')
-        plt.plot(dataSet.time, ax_pb, color='#cc0000', label='Signal filtré lowpass')
-        plt.plot(dataSet.time, ax_pb_2, color='black', label='Signal filtré filtfilt')
-        plt.grid(True, which='both')
-        plt.legend(loc="best")
-        plt.title("Filtre passe-bas du 1er ordre")
-        plt.show()
-        """
 
